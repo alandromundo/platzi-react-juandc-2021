7dee085e026656bb950b43c0115cebbd65f9d634
Alan Desktop <albertodro0909@gmail.com>
2022-11-16 15:45:18 -0600

    Components for TODO Machine project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import React from "react";
+import { TodoCounter } from './TodoCounter';
+import { TodoItem } from './TodoItem';
+import { TodoList } from './TodoList';
+import { TodoSearch } from './TodoSearch';
+import { CreateTodoButton } from './CreateTodoButton';
+
+const todos = [
+  {text: "Cortar cebolla", completed: false},
+  {text: "Freir cebolla", completed: false},
+  {text: "Sazonar cebolla", completed: false},
+]
 
 // Cuando empieza por una mayúscula significa que es un Componente
 function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {props.children} y un saludo: {props.saludo}
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <TodoCounter />
+
+      <TodoSearch />
+      
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text} />
+        ))}
+      </TodoList>
+      
+      <CreateTodoButton />
+    </React.Fragment>
   );
 }
 
