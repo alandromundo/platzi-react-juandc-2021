6136cc5110965123af0ccb3e8d0c91c06da8db8f
Alan Desktop <albertodro0909@gmail.com>
2022-11-16 12:34:27 -0600

    Differences between elements and components

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+  <App saludo="Hola">
+    Contenido children
+  </App>,
   document.getElementById('root')
 );
