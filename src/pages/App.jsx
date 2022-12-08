4bbb15dc80cbf132f05238576069d58cfd0e0242
alanMSIUbuntu <albertodro0909@gmail.com>
2022-12-08 11:34:56 -0600

    Persistencia de datos con LocalStorage

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -3,16 +3,26 @@
 import React from "react";
 import { AppUI } from "./AppUI";
 
-const defaultTodos = [
-  { text: "Cortar cebolla", completed: true },
-  { text: "Freir cebolla", completed: false },
-  { text: "Sazonar cebolla", completed: false },
-  { text: "Servir cebolla", completed: false },
-];
+// const defaultTodos = [
+//   { text: "Cortar cebolla", completed: true },
+//   { text: "Freir cebolla", completed: false },
+//   { text: "Sazonar cebolla", completed: false },
+//   { text: "Servir cebolla", completed: false },
+// ];
 
 // Cuando empieza por una mayúscula significa que es un Componente
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
+
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState("");
 
   const completedTodos = todos.filter((todo) => todo.completed).length;
@@ -30,18 +40,24 @@ function App() {
     });
   }
 
+  const saveTodos =  (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  };
+
   const toggleCompleteTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
 
     const newTodos = [...todos];
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
 
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const onDelete = (text) => {
     const todoWithoutTextTodo = todos.filter((todo) => todo.text !== text);
-    setTodos(todoWithoutTextTodo);
+    saveTodos(todoWithoutTextTodo);
   };
 
   return (
